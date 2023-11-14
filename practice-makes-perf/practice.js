efa9a3e88ba62d37819d7cc3a80ebf11ded3b23d
Katie Monfore <krae.monfore@gmail.com>
2023-11-14 13:35:01 -0600

    created new conditionals html and js to start working on

---
+++ practice-makes-perf/practice.js
@@ -0,0 +1,10 @@
+// if(/*bobber goes underwater */) {
+//     //reel in our fishies
+// } else if (/* we have been sitting more than 10 minutes*/) {
+//     recast the line
+// } else {
+//     //wait for that fishy
+// }
+
+console.log("Inside practice.js")
+
